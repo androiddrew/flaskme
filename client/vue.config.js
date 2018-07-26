5effb520ddf42dbcb3436df50e716b1ff1bdb8b8
androiddrew <bednar.andrew@gmail.com>
2018-07-26 17:25:20 -0400

    Working vue config file, but only in cmd build

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,19 +1,23 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve('../../flaskme/static'),
-  assetsDir: undefined,  //'../../flaskme/static/assets', // MUST BE RELATIVE PATH BUT IS NOT RELATIVE TO OUTPUT DIR
-  baseUrl: '/',
+  outputDir: '../flaskme/static',
+  assetsDir: undefined,
+  baseUrl: process.env.NODE_ENV === 'production'
+   ? '/static'
+   : '/',
   runtimeCompiler: undefined,
   productionSourceMap: undefined,
   parallel: undefined,
   css: undefined,
   chainWebpack: config => {
+  // Change index.html output directory
+  if (process.env.NODE_ENV === 'production'){
     config.plugin('html')
       .tap(args => {
         args[0].filename = path.resolve('../flaskme/templates/index.html');
         return args;
       });
-  },
+  }
+},
 }
-
